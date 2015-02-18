de92a486ca6872417b8a7a07595fbec4ac34d037
Hector <gilmh93@gmail.com>
2015-02-18 20:11:56 +0100

    Primer validador

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,42 @@
-console.log('\'Allo \'Allo!');
+/*$(document).ready(function(){
+
+	$("#miform").submit(function(event) {
+        event.preventDefault();
+        console.log("hola");
+
+        $("#formulario").validate({
+                // REGLAS
+                rules: {
+                    nombre: {
+                    	required: true,
+                    	minlength: 2
+                    }
+                }
+            });
+	});
+});*/
+
 $(document).ready(function(){
 	$("#formulario").validate({
-		rules: {
-			nombre: {
-				required: true, 
-				lettersonly: true
-			},
-		},
-		messages: {
-			nombre: {
-				lettersonly: "Introduce caracteres"
-			}
-		}
-	});
+        // REGLAS
+        rules: {
+            nombre: {
+            	required: true,
+            	minlength: 6,
+            	lettersonly: true
+            }
+        },
+        messages: {
+        	nombre: {
+        		required: "El nombre es necesario",
+        		minlength: "Minimo 6 caracteres"
+        	}
+        }
+    });
+
 });
+
+$.validator.addMethod("lettersonly", function(value, element) {
+	return this.optional(element) || /^[a-z]+$/i.test(value);
+}, "Solo escribir letras");
+
